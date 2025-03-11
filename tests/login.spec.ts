01e6d1d402b9547aee67d84e94d0ae02f5627eec
AnnetteCarolyn <aneta.translation@gmail.com>
2025-03-11 19:03:08 +0100

    working password validation

---
+++ tests/login.spec.ts
@@ -0,0 +1,40 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('User login to Demobank', () => {
+
+  test('successful login with correct credentials', async ({ page }) => {
+    await page.goto('https://demo-bank.vercel.app/');
+    await page.getByTestId('login-input').click();
+    await page.getByTestId('login-input').fill('testerLO');
+    await page.getByTestId('password-input').click();
+    await page.getByTestId('password-input').fill('ljkjuytt');
+    await page.getByTestId('login-button').click();
+    await page.getByTestId('user-name').click();
+
+    await expect(page.getByTestId('user-name')).toHaveText('Jan Demobankowy');
+  });
+
+  test('unsuccessful login with login with too short username', async ({ page }) => {
+    await page.goto('https://demo-bank.vercel.app/');
+    await page.getByTestId('login-input').click();
+    await page.getByTestId('login-input').fill('tester');
+    await page.getByTestId('password-input').click();
+    await page.getByTestId('error-login-id').click();
+
+    await expect(page.getByTestId('error-login-id')).toHaveText('identyfikator ma min. 8 znaków');
+  });
+});
+
+test('unsuccessful login with login with too short password', async ({ page }) => {
+  await page.goto('https://demo-bank.vercel.app/');
+
+  // page.getByTestId('login-input') == page.locator('#login_id')
+  await page.getByTestId('login-input').click(); 
+  await page.getByTestId('login-input').fill('testerPP');
+  await page.getByTestId('password-input').click();
+  await page.getByTestId('password-input').fill('wwwww');
+  await page.locator('#login_password_container i').first().click();
+
+  await expect(page.getByTestId('error-login-password')).toHaveText('hasło ma min. 8 znaków');
+  
+});
